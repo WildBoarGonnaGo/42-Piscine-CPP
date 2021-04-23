cd5404e0ec982e6bbcf74ea69aa6cd31aaeaa7f9
WildBoarGonnaGo <60565993+WildBoarGonnaGo@users.noreply.github.com>
2021-04-23 17:09:30 +0300

    d01ex02

---
+++ day01/ex02/ZombieEvent.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIEEVENT_HPP
+# define ZOMBIEEVENT_HPP
+
+#include <iostream>
+#include <string>
+#include "Zombie.hpp"
+
+class ZombieEvent
+{
+	Zombie	*setZombieType(std::string type)
+	{
+		Zombie	
+	}
+}
+
+#endif

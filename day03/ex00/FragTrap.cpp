b29d5759ef14c4e0903314b74e93f6abd0566af5
WildBoarGonnaGo <60565993+WildBoarGonnaGo@users.noreply.github.com>
2021-04-30 13:57:39 +0300

    ex00day03 progress is almost done

--- day03/ex00/FragTrap.cpp
+++ day03/ex00/FragTrap.cpp
@@ -1,6 +1,44 @@
 #include "FragTrap.hpp"
+#include <iostream>
+#
 
-FragTrap::FragTrap()
+FragTrap::FragTrap(std::string const name) : _hitPoints(100),
+	_maxHitPoints(100), _energyPoints(100), _maxEnergyPoints(100),
+	_level(1), _meleeAtckDmg(30), _rangedAtckDmg(20),
+	_armorDmgReduc(5), _name(name) { }
+
+void	FragTrap::rangedAttack(std::string const &target)
 {
-	
+	std::cout << "FR4G-TP "	<< this->_name << " attacks "
+		<< target << " at range, causing " << this->_rangedAtckDmg 
+		<< " points of damage. " <<  std::endl;
+	std::cout << "FR4G-TP " << this->_name
+		<< ": \"A whole lotta bullets in "
+		<< "just one trigger pull\"" << std::endl;
+	return ;
+}
+
+void	FragTrap::meleeAttack(std::string const &target)
+{
+	std::cout << "FR4G-TP "	<< this->_name << " attacks "
+		<< target << " at close range, causing " << this->_meleeAtckDmg 
+		<< " points of damage" << std::endl;
+	std::cout << "FR4G-TP " << this->_name
+		<< ": \"Heyyah!\"" << std::endl;
+	return ;
+}
+
+void	FragTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "FR4G-TP " << this->_name
+		<< " has taken a damage." << std::endl;
+	this->_hitPoints -= this->_meleeAtckDmg;
+	std::cout << "FR4G-TP " << this->_name 
+		<< ": \"That looks like it hurts!\"." << std::endl;
+	return ;
+}
+
+void	FragTrap::beRepaired(unsigned int amount)
+{
+	return ;
 }
--- day03/ex00/FragTrap.hpp
+++ day03/ex00/FragTrap.hpp
@@ -5,8 +5,6 @@
 class FragTrap
 {
 	public:
-		FragTrap();
-		FragTrap(FragTrap const &refFR4G_TP);
 		FragTrap(std::string const name);
 		~FragTrap();
 		void				rangedAttack(std::string const &target);
@@ -17,8 +15,10 @@ class FragTrap
 		int					_hitPoints;
 		int					_maxHitPoints;
 		int					_energyPoints;
+		int					_maxEnergyPoints;
 		int					_level;
 		std::string const	_name;
+		int					_meleeAtckDmg;
 		int					_rangedAtckDmg;
 		int					_armorDmgReduc;
 };
